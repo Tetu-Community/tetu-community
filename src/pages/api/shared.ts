a8d3e730adb5692d597e57b89754366d3fe0b1f1
adamb <adamb0123@protonmail.com>
2022-11-07 08:37:55 -0800

    wip

--- src/pages/api/shared.ts
+++ src/pages/api/shared.ts
@@ -168,3 +168,22 @@ export async function getBribeData(provider: any, proposalId: string): Promise<a
 		}
 	})
 }
+
+export async function getHiddenHandBribeData() {
+	// TODO: dynamically grab this URL
+	const res = await axios.get('https://hiddenhand.finance/_next/data/SE1CxiBzgGk69sCJJhwEQ/balancer.json')
+
+	const ret = {
+		totalVote: BigNumber(res.data.pageProps.partnerSettingsData.totalVote),
+		currentVote: BigNumber(0),
+		bribes: {},
+	}
+
+	for (const p of res.data.pageProps.proposalsData) {
+		ret.currentVote = ret.currentVote.plus(p.voteCount)
+		if (p.totalValue === 0) continue
+		ret.bribes[p.proposal] = p.totalValue
+	}
+
+	return ret
+}
--- src/pages/api/tetu-bal.ts
+++ src/pages/api/tetu-bal.ts
@@ -3,19 +3,26 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { mainnetProvider, polygonProvider } from '@/lib/ethers'
 import { CURRENT_SNAPSHOT_PROPOSAL_ID } from '@/lib/consts'
 
-import { getCoingeckoPrice, getSnapshotData, getTotalSupply, getAllGaugeAddresses, getBribeData } from './shared'
+import {
+	getCoingeckoPrice,
+	getSnapshotData,
+	getTotalSupply,
+	getAllGaugeAddresses,
+	getBribeData,
+	getHiddenHandBribeData,
+} from './shared'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	const [balPrice, tetuBalTotalSupply, veBalTotalSupply, snapshotData, bribes, allGaugeAddresses] = await Promise.all(
-		[
+	const [balPrice, tetuBalTotalSupply, veBalTotalSupply, snapshotData, bribes, allGaugeAddresses, hiddenHandBribes] =
+		await Promise.all([
 			getCoingeckoPrice('balancer'),
 			getTotalSupply(polygonProvider, '0x7fC9E0Aa043787BFad28e29632AdA302C790Ce33'),
 			getTotalSupply(mainnetProvider, '0xC128a9954e6c874eA3d62ce62B468bA073093F25'),
 			getSnapshotData(CURRENT_SNAPSHOT_PROPOSAL_ID),
 			getBribeData(polygonProvider, CURRENT_SNAPSHOT_PROPOSAL_ID),
 			getAllGaugeAddresses(),
-		]
-	)
+			getHiddenHandBribeData(),
+		])
 
 	const choicesToGaugeAddress = {}
 
@@ -32,6 +39,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 		snapshotData,
 		bribes,
 		choicesToGaugeAddress,
+		hiddenHandBribes,
 	})
 }
 
