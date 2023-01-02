1af0a192729cd03d1f2d9449ca7bc04cb176a015
adamb <adamb0123@protonmail.com>
2023-01-01 20:38:40 -0800

    use public rpc

--- src/lib/ethers.ts
+++ src/lib/ethers.ts
@@ -1,7 +1,5 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
 
-export const polygonProvider = new JsonRpcProvider(
-	`https://polygon-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
-)
+export const polygonProvider = new JsonRpcProvider(`https://polygon-rpc.com`)
 
 export const mainnetProvider = new JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`)
